70c1a11f2d5c57b9ab5d2ec01e2461e7ea48c7b8
rede8a@gmail.com <rede8a@gmail.com@2f39a724-f3cd-342e-73dd-ead0a21ed74b>
2011-02-23 07:33:57 +0000

    修改CONFIG参数 _CP_ON & _CPD_ON 防止copy

--- trunk/KES/TX_PIC16F636.asm
+++ trunk/KES/TX_PIC16F636.asm
@@ -65,7 +65,7 @@
 	#define ROMend 0x7FF
 	#include TX_eeprom.inc
 
-	__CONFIG _MCLRE_OFF & _INTRC_OSC_NOCLKOUT & _WDT_OFF & _WUREN_ON & _CP_OFF & _CPD_OFF
+	__CONFIG _MCLRE_OFF & _INTRC_OSC_NOCLKOUT & _WDT_OFF & _WUREN_ON & _CP_ON & _CPD_ON
 
       	ERRORLEVEL 0, -205,-224,-302,-305,-306	
        						; Message, Warning and Error Printed
